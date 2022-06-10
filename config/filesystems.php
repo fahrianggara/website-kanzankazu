ade564b57067f1d281a6f1b8b82fb3a438ccd8fe
zexpher <fahriangga30@gmail.com>
2022-06-10 22:42:55 +0700

    main

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
-        '/../../public_html/blog/storage' => storage_path('app/public'),
+        '/home/kanzanka/public_html/blog/storage' => storage_path('app/public'),
     ],
 
 ];
