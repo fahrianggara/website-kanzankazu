d60e40c215a00d80dee31823c746dbcc22a8eb5c
zexpher <fahriangga30@gmail.com>
2022-06-24 00:57:59 +0700

    main

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -224,9 +224,9 @@
                     value: "mel"
                 },
             ],
-            toolbar1: "fullscreen preview | link image media",
-            toolbar2: "styleselect | fontselect | fontsizeselect | bold italic | codesample emoticons | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
-            toolbar3: "restoredraft | save | insertfile undo redo",
+            toolbar1: "restoredraft | save | insertfile undo redo | link image media",
+            toolbar2: "fullscreen preview | styleselect | fontselect | fontsizeselect | bold italic | codesample emoticons | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent",
+            // toolbar3: "",
             codesample_content_css: "/public/vendor/dashboard/css/sty.css",
             // MENGKONEKKAN CONTENT GAMBAR KE FILE MANAGER
             file_picker_callback: function(callback, value, meta) {
