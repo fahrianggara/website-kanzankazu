8773576e7082da287ffa82c92f59ee157b98fe4c
zexpher <fahriangga30@gmail.com>
2022-06-23 18:13:24 +0700

    main

--- app/Http/Controllers/SitemapXmlController.php
+++ app/Http/Controllers/SitemapXmlController.php
@@ -2,16 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 use App\Models\Post;
-use Illuminate\Support\Facades\App;
+use App\Models\Tag;
+use App\Models\Tutorial;
+use App\Models\User;
 
 class SitemapXmlController extends Controller
 {
     public function index()
     {
         $posts = Post::publish()->get();
-        return response()->view('sitemap', compact('posts'))
-        ->header('Content-Type', 'text/xml');
+        $tutorials = Tutorial::whereHas('posts', function ($query) {
+            $query->publish();
+        })->get();
+        $categories = Category::whereHas('posts', function ($query) {
+            $query->publish();
+        })->get();
+        $tags = Tag::whereHas('posts', function ($query) {
+            $query->publish();
+        })->get();
+        $users = User::whereHas('posts', function ($query) {
+            $query->publish();
+        })->get();
+
+        return response()
+            ->view('sitemap', compact('posts', 'tutorials', 'categories', 'tags', 'users'))
+            ->header('Content-Type', 'text/xml');
     }
 }
--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,9 +1,91 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <url>
+        <loc>{{ route('blog.home') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('blog.categories') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('blog.tags') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('blog.tutorials') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('blog.authors') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('login') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
+    <url>
+        <loc>{{ route('register') }}</loc>
+        <lastmod>2022-06-23T11:09:30+00:00</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>0.8</priority>
+    </url>
     @foreach ($posts as $post)
         <url>
             <loc>{{ route('blog.detail', ['slug' => $post->slug]) }}</loc>
             <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+    @foreach ($tutorials as $item)
+        <url>
+            <loc>{{ route('blog.posts.tutorials', ['slug' => $item->slug]) }}</loc>
+            <lastmod>{{ $item->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+    @foreach ($categories as $item)
+        <url>
+            <loc>{{ route('blog.posts.categories', ['slug' => $item->slug]) }}</loc>
+            <lastmod>{{ $item->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+    @foreach ($tags as $item)
+        <url>
+            <loc>{{ route('blog.posts.tags', ['slug' => $item->slug]) }}</loc>
+            <lastmod>{{ $item->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+    @foreach ($users as $user)
+        <url>
+            <loc>{{ route('blog.author', ['author' => $user->slug]) }}</loc>
+            <lastmod>{{ $user->created_at->tz('UTC')->toAtomString() }}</lastmod>
+            <changefreq>weekly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
+    @foreach ($users as $user)
+        <url>
+            <loc>{{ route('blog.posts.tutorials.author', ['user' => $user->slug, 'slug' => $user->tutorials->first()->slug]) }}</loc>
+            <lastmod>{{ $user->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>weekly</changefreq>
             <priority>0.8</priority>
         </url>
