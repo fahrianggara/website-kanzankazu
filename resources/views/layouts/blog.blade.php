d71d1b538183be314cd53d6f3ddaac7c53215d00
zexpher <fahriangga30@gmail.com>
2022-07-01 00:02:38 +0700

    main

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -51,7 +51,7 @@
     {{-- jQuery Ui --}}
     <link rel="stylesheet" href="{{ asset('vendor/blog/assets/jquery-ui/jquery-ui.css') }}">
     {{-- Main CSS --}}
-    <link rel="stylesheet" href="{{ asset('vendor/blog/css/sty.css') }}">
+    <link rel="stylesheet" href="{{ asset('vendor/blog/css/st.css') }}">
     {{-- CSS EXT --}}
     @stack('css-external')
     {{-- CSS INT --}}
