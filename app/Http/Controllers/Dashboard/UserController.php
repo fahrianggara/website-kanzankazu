6a755aa2ee0e82ab6ef047be98087d7632245949
zexpher <fahriangga30@gmail.com>
2022-07-05 03:19:17 +0700

    main

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -56,7 +56,7 @@ class UserController extends Controller
         }
 
         return view('dashboard.manage-users.users.index', [
-            'users' => $users->paginate(10)->withQueryString(),
+            'users' => $users->paginate(50)->withQueryString(),
             'statusSelected' => $statusSelected,
         ]);
     }
