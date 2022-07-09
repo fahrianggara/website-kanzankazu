1f37b4bcc5e51fa858ab7e98e4b834356c6a6e01
zexpher <fahriangga30@gmail.com>
2022-07-09 20:58:39 +0700

    main

--- firebase-sign-in.js
+++ firebase-sign-in.js
@@ -8,6 +8,7 @@ import {
     GoogleAuthProvider,
     GithubAuthProvider,
     createUserWithEmailAndPassword,
+    signInAnonymously,
     getAuth,
     signInWithPopup
 } from "https://www.gstatic.com/firebasejs/9.8.4/firebase-auth.js";
@@ -30,15 +31,100 @@ const githubProvider = new GithubAuthProvider();
 const auth = getAuth();
 auth.languageCode = 'id';
 
+$('#signWithAnonym').on('click', function() {
+    signInAnonymously(auth)
+        .then((result) => {
+
+            const user = result.user;
+
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $.ajax({
+                type: "POST",
+                url: "{{ route('anonymous.login') }}",
+                data: {
+                    uid: user.uid,
+                },
+                beforeSend: function() {
+                    $('#signWithGoogle').addClass('disable');
+                    $('#signWithGithub').addClass('disable');
+
+                    $('#signWithAnonym').addClass('disable');
+                    $('#signWithAnonym').html('<i class="fas fa-spin fa-spinner"></i>');
+                    $('#tooltipAnonym').tooltip('dispose').attr('title',
+                        'Pengecekan..');
+                    $('#tooltipAnonym').tooltip('show')
+                },
+                success: function(data) {
+                    if (data.status == 200) {
+
+                        $('#signWithAnonym').html('<i class="uil uil-check"></i>');
+                        $('#tooltipAnonym').tooltip('dispose').attr('title',
+                            'Status OK..');
+                        $('#tooltipAnonym').tooltip('show')
+
+                        setTimeout(function() {
+                            $('#signWithAnonym').html(
+                                '<i class="fas fa-spin fa-spinner"></i>');
+
+                            $('#tooltipAnonym').tooltip('dispose').attr('title',
+                                'Mengalihkan ke Dashboard...');
+                            $('#tooltipAnonym').tooltip('show');
+                        }, 1700);
+
+                        alertify
+                            .delay(3500)
+                            .log(data.msg);
+
+                        setTimeout((function() {
+                            window.location.href = data.redirect;
+                        }), 3000);
+                    } else if (response.status == 403) {
+                        alertify
+                            .delay(4000)
+                            .error(data.msg);
+
+                        $('#signWithAnonym').html('<i class="uil uil-ban"></i>');
+                        $('#tooltipAnonym').tooltip('dispose').attr('title',
+                            data.msg);
+                        $('#tooltipAnonym').tooltip('show');
+
+                        setTimeout(function() {
+                            $('#signWithAnonym').html(
+                                '<i class="fas fa-spin fa-spinner"></i>');
+                            $('#tooltipAnonym').tooltip('dispose').attr('title',
+                                'Muat ulang halaman..');
+                            $('#tooltipAnonym').tooltip('show');
+                        }, 1700);
+
+                        setTimeout((function() {
+                            window.location.href = data.redirect;
+                        }), 3000);
+                    }
+                },
+                error: function(xhr, ajaxOptions, thrownError) {
+                    alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                }
+            });
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+        });
+
+});
+
 $('#signWithGithub').click(function() {
     signInWithPopup(auth, githubProvider)
         .then((result) => {
 
             const credential = GithubAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
-
             const user = result.user;
-            console.log(user);
 
             $.ajaxSetup({
                 headers: {
@@ -57,6 +143,10 @@ $('#signWithGithub').click(function() {
                     uid: user.uid
                 },
                 beforeSend: function() {
+                    $('#signWithGoogle').addClass('disable');
+                    $('#signWithAnonym').addClass('disable');
+
+                    $('#signWithGithub').addClass('disable');
                     $('#signWithGithub').html('<i class="fas fa-spin fa-spinner"></i>');
                     $('#tooltipGithub').tooltip('dispose').attr('title',
                         'Pengecekan..');
@@ -136,28 +226,26 @@ $('#signWithGithub').click(function() {
 
             const errorCode = error.code;
             const errorMessage = error.message;
-
             const email = error.customData.email;
-
             const credential = GithubAuthProvider.credentialFromError(error);
 
             if (errorCode == 'auth/user-disabled') {
                 alertify.okBtn("OK").alert(
-                    'Maaf.. akun kamu telah kami blokir. Silahkan kontak admin untuk informasi lebih lanjut :)');
+                    'Maaf.. akun kamu telah kami blokir. Silahkan kontak admin untuk informasi lebih lanjut :)'
+                );
             }
         });
 });
 
-$('#signWithGoogle').click(function() {
+$('#signWithGoogle').on('click', function(e) {
+    e.preventDefault();
+
     signInWithPopup(auth, googleProvider)
         .then((result) => {
 
             const credential = GoogleAuthProvider.credentialFromResult(result);
             const token = credential.accessToken;
-
             const user = result.user;
-
-            console.log(user);
 
             $.ajaxSetup({
                 headers: {
@@ -176,7 +264,12 @@ $('#signWithGoogle').click(function() {
                     disable: user.reloadUserInfo.disabled
                 },
                 beforeSend: function() {
-                    $('#signWithGoogle').html('<i class="fas fa-spin fa-spinner"></i>');
+                    $('#signWithGithub').addClass('disable');
+                    $('#signWithAnonym').addClass('disable');
+
+                    $('#signWithGoogle').addClass('disable');
+                    $('#signWithGoogle').html(
+                        '<i class="fas fa-spin fa-spinner" disabled="disabled"></i>');
                     $('#tooltipGoogle').tooltip('dispose').attr('title',
                         'Pengecekan..');
                     $('#tooltipGoogle').tooltip('show')
@@ -185,6 +278,7 @@ $('#signWithGoogle').click(function() {
                     if (data.status == 200) {
 
                         $('#signWithGoogle').html('<i class="uil uil-check"></i>');
+
                         $('#tooltipGoogle').tooltip('dispose').attr('title',
                             'Status OK..');
                         $('#tooltipGoogle').tooltip('show')
@@ -212,6 +306,7 @@ $('#signWithGoogle').click(function() {
                             .error(data.msg);
 
                         $('#signWithGoogle').html('<i class="uil uil-ban"></i>');
+
                         $('#tooltipGoogle').tooltip('dispose').attr('title',
                             data.msg);
                         $('#tooltipGoogle').tooltip('show');
@@ -257,14 +352,13 @@ $('#signWithGoogle').click(function() {
 
             const errorCode = error.code;
             const errorMessage = error.message;
-
             const email = error.customData;
-
             const credential = GoogleAuthProvider.credentialFromError(error);
 
             if (errorCode == 'auth/user-disabled') {
                 alertify.okBtn("OK").alert(
-                    'Maaf.. akun kamu telah kami blokir. Silahkan kontak admin untuk informasi lebih lanjut :)');
+                    'Maaf.. akun kamu telah kami blokir. Silahkan kontak admin untuk informasi lebih lanjut :)'
+                );
             }
         });
 });
