bb3fba62510a69f7fb64cb35cc1d93335cb9061f
zexpher <fahriangga30@gmail.com>
2022-11-07 19:53:30 +0700

    update

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -40,7 +40,7 @@ class BackupDatabase extends Command
     {
         $filename = "backup_" . Carbon::now()->format('d_m_Y') . ".gz";
         $path = "vendor/dashboard/documents/backup";
-        $command = "mysqldump --user=" . env('DB_USERNAME') . ' --password=jxkfXG@f(VDX --host=' . env('DB_HOST') . " " . env("DB_DATABASE") . " > " . $path . '/' . $filename;
+        $command = "mysqldump --user=" . env('DB_USERNAME') . " --password='" . env('DB_PASSWORD') ."' --host=" . env('DB_HOST') . " " . env("DB_DATABASE") . " > " . $path . '/' . $filename;
 
         exec($command);
     }
