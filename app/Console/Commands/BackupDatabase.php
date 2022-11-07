2d237039d6efc5d6dedf9fd248ebb9116c2c706b
zexpher <fahriangga30@gmail.com>
2022-11-07 19:38:53 +0700

    backup

--- app/Console/Commands/BackupDatabase.php
+++ app/Console/Commands/BackupDatabase.php
@@ -38,11 +38,9 @@ class BackupDatabase extends Command
      */
     public function handle()
     {
-        $filename = "backup_" . Carbon::now()->format('Y-m-d') . ".gz";
+        $filename = "backup_" . Carbon::now()->format('d_m_Y') . ".gz";
         $path = "vendor/dashboard/documents/backup/";
-        $command = "mysqldump --user=" . env('DB_USERNAME')
-            . " --password=" . env("DB_PASSWORD") . " --host=" . env('DB_HOST')
-            . " " . env("DB_DATABASE") . " > " . $path . $filename;
+        $command = "mysqldump --user=" . env('DB_USERNAME') . " --password=" . env("DB_PASSWORD") . " --host=" . env('DB_HOST') . " " . env("DB_DATABASE") . " > " . $path . $filename;
 
         exec($command);
     }
